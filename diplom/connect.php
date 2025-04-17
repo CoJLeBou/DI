584f7d51ac1cf73ce1baded5f4d7e078dfbe4706
CoJLeBou <dlinhic@gmail.com>
2025-04-17 20:12:47 +0700

    Delete diplom/connect.php

--- diplom/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname = "localhost"; //127.0.0.1
-    $username = "root";
-    $password = "";
-    $database = "continental";
-
-    $link = mysqli_connect($hostname, $username, $password, $database);
-?>
