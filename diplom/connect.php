2995920e194451fbaa3ff83e389b7f9fa37d9bc8
CoJLeBou <dlinhic@gmail.com>
2025-04-17 20:01:19 +0700

    Add files via upload

---
+++ diplom/connect.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = "localhost"; //127.0.0.1
+    $username = "root";
+    $password = "";
+    $database = "continental";
+
+    $link = mysqli_connect($hostname, $username, $password, $database);
+?>
